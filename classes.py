507a70ad5abed39c6205f2ec7d6cbc26948af176
TrevorMcGarrah <>
2022-11-03 19:18:03 +0000

    Merged inventory system into the main game

--- classes.py
+++ classes.py
@@ -1,17 +1,78 @@
-class Player:
-    def __init__(self, name, gender, attr, gold = 15, fame = 5, love = 5, weapon = None, health = 100, gen = 1):
-        self.name = name
-        self.gender = gender
-        self.attr = attr
-        self.gold = gold
-        self.fame = fame
-        self.love = love
-        self.weapon = weapon
-        self.health = health
-        self.gen = gen
+from os import system
+from getkey import keys, getkey
+import time
 
-    def change_weapon(self, weapon):
-        self.weapon = weapon
+class Player:
+  def __init__(self,
+               name,
+               gender,
+               attr,
+               inventory=[]):
+    self.name = name
+    self.gender = gender
+    self.attr = attr
+    self.gold = 15
+    self.fame = 5
+    self.love = 5
+    self.weapon = Weapon("fist", 0, 10)
+    self.health = 100
+    self.max_health = 100
+    self.gen = 1
+    self.inventory = inventory
+    
+  def change_weapon(self, weapon):
+    self.weapon = weapon
+
+  def equip_item(self, inven, num):
+      for i in range(5):
+        if i == num - 1:
+          
+          self.weapon = self.inventory[num-1]
+          print(f"You equipped {self.weapon.name}!")
+          time.sleep(2)
+          system("clear")
+    
+    
+  def remove_item(self, dropping, inven):
+    sure = input("are you sure?: [y]es or [n]o\n\n")
+    
+    if sure.upper() == "Y":
+      for i in range(5):
+        if i == dropping - 1:
+          self.inventory.pop(dropping-1)
+          system("clear")
+      self.inventory.append("empty")
+    else:
+      print("Phew, that was close!")
+      time.sleep(2)
+      system("clear")
+        
+  def view_inventory(self, inv):
+    print(f"EQUIPPED WEAPON: {self.weapon.name}")#add equipped_item variable somehow
+    print("EQUIPMENT:")
+    for i, item in enumerate(self.inventory):
+      if item != "empty":
+        print(f"{i+1}. {item.name}")
+      else:
+          print(f"{i+1}. open slot")
+      
+    print("\nPress [e] to equip an item, press [r] or [delete] to remove an item, press [i] or [esc] to close the menu.\n\n")
+
+    inv_button = getkey()
+    # drop_item
+    # display_attributes
+    # equip_item
+
+    if inv_button == keys.ESCAPE or inv_button.upper() == "I":
+      system("clear")
+    elif inv_button.upper() == "R" or inv_button == keys.DELETE:
+        dropping = input("Enter the inventory index to drop (1-5): ")
+        dropping = int(dropping)
+        Player.remove_item(self, dropping, inv)
+    elif inv_button.upper() == "E" :
+        equip = input("Enter the inventory index of the item you'd like to equip (1-5): ")
+        equip = int(equip)
+        Player.equip_item(self, inv, equip)
 
 
 class Item:
---
+++ inventory.py
@@ -0,0 +1,51 @@
+# View inventory DONE
+#while true loop
+#button press "i"
+#display DONE ALL
+
+# Use item in the inventory DONE
+#button press 'i'
+#item info
+
+# Drop item DONE
+#inventory movement NOT NECESSARY
+#button press "DELETE" w/ "are you sure?" SUCCESS
+#item removal SUCCESS
+
+# Display all player attributes
+#display inventory DONE
+#call player variables 
+#weapon type too (LATER ONCE EVERYTHING IMPORTED)
+#show potion effects
+from os import system
+from getkey import keys, getkey
+import time
+
+
+
+# player = Player("Bob", "male", "male")
+
+while True:
+  print("You are in the map, Press i to open and close inventory.")
+  uinput = getkey()
+  if uinput.upper() == "I":
+    system("clear")
+    if len(player.inventory) > 0:
+      player.view_inventory(player.inventory)
+
+    elif len(player.inventory) == 0:
+      print("Whoops, all nothing!")
+      time.sleep(2)
+      system("clear")
+
+        
+        
+        #ACTUALLY USELESS
+        # Dustin's Immortal Legendary Forever Stupidity or the DILFS rule
+    # As a clause of Dustin's Immortal Stupidity, abuse to Dustin is allowed
+    # In an additional clause, We can make Dustin feel stupid.
+# def something():
+#   def something_else():
+
+        
+#🥫ಠ__ಠ   еру иуые цфн ещ сщву ершы зкщпкфь шы ещ тще сщву ше фе фддю
